edbe07ba15e307b3ea00421564ab523dfe87ba38
Alex Perez <alex.f.perez92@gmail.com>
2023-05-03 12:10:07 -0500

    added delete function and main menu

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Contact c1 = new Contact();
-        c1.addContact();
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+        do {
+            c1.mainMenu();
+            choice = Integer.parseInt(scanner.nextLine());
+            switch (choice) {
+                case 1:
+                    c1.allContacts();
+                    break;
+                case 2:
+                    c1.addContact();
+                    break;
+                case 3:
+                    c1.searchContact();
+                    break;
+                case 4:
+                    c1.deleteContact();
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
+            }
+        } while (choice != 5);
     }
 }
